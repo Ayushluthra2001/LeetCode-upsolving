71af4a92bcdba43d6fcc6271f171d6cc2cc0b8cb
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-02-17 20:13:11 +0530

    Create 100221. Maximum Number of Operations With the Same Score I.cpp

---
+++ 100221. Maximum Number of Operations With the Same Score I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums) {
+        if(nums.size()<=2) return 1;
+        int sum=nums[0]+nums[1];
+        int count=1;
+        
+        for(int i=2;i<nums.size()-1;i+=2){
+            if(nums[i]+nums[i+1]!=sum) return count;
+            else count++;
+        }
+        return count;
+    }
+};
