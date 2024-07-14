aa2a07e4fc33f026e2490f4cf640033af3cd9ac5
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-07-14 10:21:47 +0530

    Create Minimum Cost for Cutting Cake II.cpp

---
+++ Minimum Cost for Cutting Cake II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+      
+  
+           sort(horizontalCut.begin(), horizontalCut.end(), greater<int>());
+           sort(verticalCut.begin(), verticalCut.end(), greater<int>());
+
+        int horizontal = 1, vertical = 1;
+        int i = 0, j = 0;
+        long long ans = 0;
+
+       
+        while (i < m - 1 && j < n - 1) {
+            if (horizontalCut[i] > verticalCut[j]) {
+                ans += horizontalCut[i] * vertical;
+                horizontal++;
+                i++;
+            } else {
+                ans += verticalCut[j] * horizontal;
+                vertical++;
+                j++;
+            }
+        }
+        while (i < m - 1) {
+            ans += horizontalCut[i] * vertical;
+            i++;
+        }
+
+        
+        while (j < n - 1) {
+            ans += verticalCut[j] * horizontal;
+            j++;
+        }
+
+        return ans;
+    }
+    
+};
+    
